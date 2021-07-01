fac29423456390ef39368c2a34954da52539b9d5
Daniel Washington <danielvertigo@hotmail.com>
2021-07-01 10:32:29 -0300

    Files

--- PartiallyHiddenString/ViewController.swift
+++ PartiallyHiddenString/ViewController.swift
@@ -5,6 +5,25 @@
 //  Created by Daniel Washington Ignacio on 01/07/21.
 //
 
+/*
+ Create a function that takes a phrase and transforms each word using the following rules:
+
+ Keep first and last character the same.
+ Transform middle characters into a dash -.
+ Examples
+
+ partiallyHide("skies were pretty") ➞ "s---s w--e p----y"
+
+ partiallyHide("red is not my color") ➞ "r-d is n-t my c---r"
+
+ partiallyHide("She rolled her eyes") ➞ "S-e r----d h-r e--s"
+
+ partiallyHide("Harry went to fight the basilisk") ➞ "H---y w--t to f---t t-e b------k"
+ Notes
+
+ Words with two or fewer letters should not be hidden at all.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +31,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.partiallyHide("skies were pretty"))
+        print(self.partiallyHide("red is not my color"))
+        print(self.partiallyHide("She rolled her eyes"))
+        print(self.partiallyHide("Harry went to fight the basilisk"))
     }
 
+    func partiallyHide(_ phrase: String) -> String {
+        let replaced = phrase.replacingOccurrences(of: "(?<=\\S)\\S(?=\\S)", with: "-", options: .regularExpression)
+            return replaced
+    }
 
 }
 
